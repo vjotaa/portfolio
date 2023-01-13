c5179812f5e0fe0d33f463e880b68005ae372958
Victor Manrique <ivictor@MacBook-Pro-de-Victor-2.local>
2023-01-12 19:01:24 -0500

    feat: init portfolio

--- .svelte-kit/output/server/index.js
+++ .svelte-kit/output/server/index.js
@@ -2547,7 +2547,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1673567522935"
+      version: "1673567963442"
     };
   }
   /**
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import adapter from "@sveltejs/adapter-auto";
+import netlify from "@sveltejs/adapter-netlify";
 import preprocess from "svelte-preprocess";
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [
     preprocess({
@@ -8,7 +9,7 @@ const config = {
     }),
   ],
   kit: {
-    adapter: adapter(),
+    adapter: netlify(),
   },
 };
 

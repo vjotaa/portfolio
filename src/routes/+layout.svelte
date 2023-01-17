0fe0fb710319fb9f5c6e132bf1b8ad21a68750ae
Victor Manrique <ivictor@MacBook-Pro-de-Victor-2.local>
2023-01-16 22:04:00 -0500

    feat: added uppercase to text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -69,7 +69,7 @@
             <a
               href="/illustrations"
               class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >illustrations</a
+              >Illustrations</a
             >
 
             <a
@@ -97,7 +97,7 @@
           on:click={() => (open = !open)}
           href="/illustrations"
           class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >illustrations</a
+          >Illustrations</a
         >
 
         <a
